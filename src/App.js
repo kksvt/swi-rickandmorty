82d61ddf9672091ca6204d1a97edee18252df0fd
Krzysztof <krzysztof.swiatek@student.uj.edu.pl>
2025-02-16 21:40:48 +0100

    final touches

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import CharacterList from './components/CharacterList'
